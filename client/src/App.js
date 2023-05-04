063df697320872cd7ec5324d8270ee0e76de8321
Keeevini <kevin.ni2000@gmail.com>
2023-05-03 17:19:37 -0700

    Initializing header

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 // import { } from './container';
 import { Navbar } from './components';
+import { Header } from './containers';
 import './App.css';
 
 const App = () => {
 	return (
 		<div>
 			<Navbar />
+			<Header />
 		</div>
 	)
 }
