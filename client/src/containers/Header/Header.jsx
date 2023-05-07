02d8358748cb88d9f69da4e55d256e524b4843e3
Keeevini <kevin.ni2000@gmail.com>
2023-05-07 16:33:55 -0700

    Fixed a few bugs and resizing errors

--- client/src/containers/Header/Header.jsx
+++ client/src/containers/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {SubHeading } from '../../components';
+import { SubHeading } from '../../components';
 import { images } from '../../constants';
 import './Header.css';
 
@@ -9,7 +9,7 @@ const Header = () => (
 		<div className="app__wrapper_info">
 			<SubHeading title="Chase the new flavour"/>
 			<h1 className="app__header-h1">The Key to Fine Dining</h1>
-			<p className="p__opensans" style={{margin: '2rem 0'}}>A wonderful explanation</p>
+			<p className="p__opensans" style={{ margin: '2rem 0' }}>A wonderful explanation</p>
 			<button type="button" className="custom__button">Explore Menu</button>
 		</div>
 
