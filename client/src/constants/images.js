3f6caa9cda5bbe172d2e7f0b1a6afd814d0d39ec
Keeevini <kevin.ni2000@gmail.com>
2023-05-07 17:36:01 -0700

    Added left section

--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -1,6 +1,8 @@
 import diamond_net_title from '../assets/diamond_net_title.png';
 import spoon from '../assets/spoon.png';
 import welcome from '../assets/welcome.png';
+import G from '../assets/G.png';
+import bg from '../assets/bg.png';
 
 export default {
 
@@ -8,4 +10,6 @@ export default {
 	diamond_net_title,
 	spoon,
 	welcome,
+	G,
+	bg,
 };
