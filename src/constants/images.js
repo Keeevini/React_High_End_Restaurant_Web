89281c2e616a0772e945d107716819653f39b272
Keeevini <kevin.ni2000@gmail.com>
2023-05-07 20:37:24 -0700

    Added the about us section

--- src/constants/images.js
+++ src/constants/images.js
@@ -3,10 +3,11 @@ import spoon from '../assets/spoon.png';
 import welcome from '../assets/welcome.png';
 import G from '../assets/G.png';
 import bg from '../assets/bg.png';
+import knife from '../assets/knife.png';
 
 export default {
 
-
+	knife,
 	diamond_net_title,
 	spoon,
 	welcome,
