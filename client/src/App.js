8e621da2cbe4e08351f00348a9dbb3741610e6d6
Keeevini <kevin.ni2000@gmail.com>
2023-05-07 20:51:58 -0700

    Initializing Special Menu

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 // import { } from './container';
 import { Navbar } from './components';
-import { AboutUs, Header } from './containers';
+import { AboutUs, Header, SpecialMenu } from './containers';
 import './App.css';
 
 const App = () => {
@@ -11,6 +11,7 @@ const App = () => {
 			<Navbar />
 			<Header />
 			<AboutUs />
+			<SpecialMenu />
 		</div>
 	)
 }
---
+++ client/src/containers/Menu/SpecialMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './SpecialMenu.css';
+
+const SpecialMenu = () => (
+	<div>
+		Special Menu
+	</div>
+
+)
+
+export default SpecialMenu
