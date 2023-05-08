bfaab8ec9a199425daee6afdc9c826ab3998ac50
Keeevini <kevin.ni2000@gmail.com>
2023-05-07 17:36:01 -0700

    Added left section

--- src/containers/AboutUs/AboutUs.jsx
+++ src/containers/AboutUs/AboutUs.jsx
@@ -4,8 +4,21 @@ import { images } from '../../constants';
 import './AboutUs.css';
 
 const AboutUs = () => (
-	<div className="app__aboutus app_bg flex__center section__padding" id="about">
+	<div className="app__aboutus app__bg flex__center section__padding" id="about">
+		<div className="app__aboutus-overlay flex__center">
+			<img src={images.G} alt="g letter" />
+		</div>
 
+		<div className="app__aboutus-content flex__center">
+			<div className="app__aboutus-content_about">
+				<h1 className="headtext__cormorant">About Us</h1>
+				<img src={images.spoon} alt="about spoon" className="spoon_img" />
+				<p className="p__opensans">zzzzzzzzzzzzzzzzz</p>
+				<button type="button" className="custom__button">Know More</button>
+
+			</div>
+
+		</div>
 	</div>
 
 
