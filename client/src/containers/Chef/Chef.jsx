88a0f0dc34dd987160ad99eca6e6a817b6cb3002
Keeevini <kevin.ni2000@gmail.com>
2023-05-14 21:54:52 -0700

    Adding headers

--- client/src/containers/Chef/Chef.jsx
+++ client/src/containers/Chef/Chef.jsx
@@ -5,8 +5,16 @@ import { images, data } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-	<div>
-		Chef
+	<div className="app__bg app__wrapper section__padding">
+		<div className="app__wrapper_img app__wrapper_img-reverse">
+			<img src={images.chef} alt="chef" />
+		<div>
+
+		<div className="app__wrapper_info">
+			<SubHeading title="Chef's Word" />
+			<h1 className="headtext__cormorant">What we believe in</h1>
+		</div>
+		
 	</div>
 );
 
