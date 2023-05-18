643bf8c7806cbe05b836c744f359d94317dc2b15
Keeevini <kevin.ni2000@gmail.com>
2023-05-17 23:47:15 -0700

    Updates

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Navbar from './Navbar/Navbar';
-import SubHeading from './SubHeading/SubHeading';
-import MenuItem from './MenuItem/MenuItem';
-
-export {
-	Navbar,
-	SubHeading,
-	MenuItem,
-};
