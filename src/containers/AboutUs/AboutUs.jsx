72970302aabd3103ca34cbbe719fb7c62f9f5f5c
Keeevini <kevin.ni2000@gmail.com>
2023-05-17 21:32:20 -0700

    Changed the about us section wording

--- src/containers/AboutUs/AboutUs.jsx
+++ src/containers/AboutUs/AboutUs.jsx
@@ -13,7 +13,7 @@ const AboutUs = () => (
 			<div className="app__aboutus-content_about">
 				<h1 className="headtext__cormorant">About Us</h1>
 				<img src={images.spoon} alt="about_spoon" className="spoon_img" />
-				<p className="p__opensans">Diamond Net is more than just a restaurant - it's a destination for sushi lovers. Our menu features a diverse selection of sushi and other Japanese dishes, all prepared with an unparalleled attention to detail. Whether you're looking for a romantic evening out or a memorable dining experience with friends and family, Diamond Net is the perfect choice.</p>
+				<p className="p__opensans">Diamond Net is more than just a restaurant - it's a destination for sushi lovers. Our menu features a diverse selection of sushi and other Japanese dishes, all prepared with an unparalleled attention to detail. Whether you're looking for a romantic evening out, a memorable dining experience with friends and family, or an exploration of the artistry and flavors of authentic Japanese cuisine, Diamond Net offers an inviting ambiance and exceptional service to elevate your culinary journey.</p>
 				<button type="button" className="custom__button">Know More</button>
 
 			</div>
@@ -25,7 +25,7 @@ const AboutUs = () => (
 			<div className="app__aboutus-content_history">
 				<h1 className="headtext__cormorant">Our History</h1>
 				<img src={images.spoon} alt="about_spoon" className="spoon_img" />
-				<p className="p__opensans">Our story began with a simple mission - to create a sushi restaurant that would stand apart from the rest. Over the years, we have refined our menu, honed our techniques, and created an atmosphere that is truly unique. Today, Diamond Net is proud to be a destination for sushi enthusiasts from around the world.</p>
+				<p className="p__opensans">Our story began with a simple mission - to create a sushi restaurant that would stand apart from the rest. Over the years, we have refined our menu, honed our techniques, and created an atmosphere that is truly unique. Today, Diamond Net is proud to be a destination for sushi enthusiasts from around the world, where our dedication to culinary excellence and unwavering commitment to exceptional service continue to shape unforgettable dining experiences.</p>
 				<button type="button" className="custom__button">Know More</button>
 
 			</div>
