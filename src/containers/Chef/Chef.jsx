36bbac2aeccfccec0074702b8c7ea37b4d1d02ec
Keeevini <kevin.ni2000@gmail.com>
2023-05-17 21:06:04 -0700

    Fixed sizing

--- src/containers/Chef/Chef.jsx
+++ src/containers/Chef/Chef.jsx
@@ -16,12 +16,12 @@ const Chef = () => (
 			<div className="app__chef-content">
 				<div className="app__chef-content_quote">
 					<img src={images.quote} alt="quote" />
-					<p className="p__opensans">TBD</p>
+					<p className="p__opensans">With a deep appreciation for the art of sushi making, we strive to create a culinary</p>
 				</div>
-				<p className="p__opensans">TBD</p>
+				<p className="p__opensans">experience that transcends taste alone. Our team of skilled chefs is dedicated to delivering the finest quality sushi, prepared with precision and creativity. We embrace the principles of omotenashi, providing warm hospitality and personalized service to ensure that every guest feels welcomed and cared for. We invite you to join us on a journey of exquisite flavors and impeccable craftsmanship, where every dish tells a story of passion and dedication.</p>
 			</div>
 			<div className="app__chef-sign">
-				<p>Name</p>
+				<p>Kevin Luo</p>
 				<p className="p__opensans">Chef & Founder</p>
 				<img src={images.signature} alt="signature" />
 			</div>
