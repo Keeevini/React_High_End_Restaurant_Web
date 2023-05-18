cd022e2ef47a1eaa2a07227dbd90686c13a92b0c
Keeevini <kevin.ni2000@gmail.com>
2023-05-17 21:07:47 -0700

    Moved a line

--- src/containers/Chef/Chef.jsx
+++ src/containers/Chef/Chef.jsx
@@ -16,9 +16,9 @@ const Chef = () => (
 			<div className="app__chef-content">
 				<div className="app__chef-content_quote">
 					<img src={images.quote} alt="quote" />
-					<p className="p__opensans">With a deep appreciation for the art of sushi making, we strive to create a culinary</p>
+					<p className="p__opensans">With a deep appreciation for the art of sushi making, we strive to</p>
 				</div>
-				<p className="p__opensans">experience that transcends taste alone. Our team of skilled chefs is dedicated to delivering the finest quality sushi, prepared with precision and creativity. We embrace the principles of omotenashi, providing warm hospitality and personalized service to ensure that every guest feels welcomed and cared for. We invite you to join us on a journey of exquisite flavors and impeccable craftsmanship, where every dish tells a story of passion and dedication.</p>
+				<p className="p__opensans">create a culinary experience that transcends taste alone. Our team of skilled chefs is dedicated to delivering the finest quality sushi, prepared with precision and creativity. We embrace the principles of omotenashi, providing warm hospitality and personalized service to ensure that every guest feels welcomed and cared for. We invite you to join us on a journey of exquisite flavors and impeccable craftsmanship, where every dish tells a story of passion and dedication.</p>
 			</div>
 			<div className="app__chef-sign">
 				<p>Kevin Luo</p>
